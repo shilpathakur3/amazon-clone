e4889e037aeb22f0c248cf82356c9a99983ab9ba
Shilpa Singh <singhshilpa9920@gmail.com>
2020-09-14 16:40:36 +0530

    my changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css';
-// import { } from "react-router-dom"
-// import Header from './Header'
-// import { useStateValue } from "./StateProvider"
-// import { auth } from "./firebase"
-import Login from './Login'
+import Header from './Header'
 
 
 function App() {
   return (
     //BEM
     <div className="app">
-     {/* <Header /> */}
-     {/* Home */}
-     <Login />
+      < Header />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Header.css";
-// import SearchIcon from "@material-ui/icons/Search"
+import SearchIcon from "@material-ui/icons/Search"
 
 function Header() {
     return(
@@ -12,7 +12,7 @@ function Header() {
           <div className="header__search">
           <input className="header__searchInput"
           type="text" />
-          {/* <SearchIcon className="header__searchIcon" /> */}
+          <SearchIcon className="header__searchIcon" />
           </div>
 
           <div className="header__nav">
