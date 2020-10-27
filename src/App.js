2a5fcf28f493a5b33f707c0efe9e1ade4cbbc58d
[Shilpa Singh] <[singhshipa9920@gmail.com]>
2020-10-27 13:18:37 +0530

    yes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           </Route>
           <Route path="/">
             <Header />
-            <h1> Home Page....</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
